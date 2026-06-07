74bdf3415e2b9dabbe19617864dbca3749f23742
msadley <mendes.adley@gmail.com>
2026-06-07 20:11:29 -0300

    zsh/starship: remove top empty line from prompt

---
+++ zsh/dot-config/zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias l="ls -la"
+
+alias redock="docker compose down && docker compose up"
+alias redockb="docker compose down && docker compose up --build"
+alias redockv="docker compose down -v && docker compose"
+
+alias ..="cd .."
+alias ...="cd ..."
+alias ....="cd ...."
+
